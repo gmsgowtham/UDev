c3905b7cf5e36e6f59af2463c847d4cdcd4a0f6d
Gowtham G <webappsbygowtham@gmail.com>
2023-04-27 12:51:17 +0530

    chore: use ts for index

--- index.ts
+++ index.ts
@@ -1,11 +1,5 @@
-/**
- * @format
- */
-
-import { AppRegistry, LogBox } from "react-native";
+import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
-
-LogBox.ignoreAllLogs();
 
 AppRegistry.registerComponent(appName, () => App);

847dee8c85d191662d55cb3e69623883d4fad6c5
Gowtham G <webappsbygowtham@gmail.com>
2023-06-18 16:28:19 +0530

    chore: reuse common types

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,4 +1,4 @@
-export interface ApiArticleFeedItem {
+interface ApiArticleCommon {
 	type_of: string;
 	id: number;
 	title: string;
@@ -21,17 +21,8 @@ export interface ApiArticleFeedItem {
 	published_at: string;
 	last_comment_at: string;
 	reading_time_minutes: number;
-	tag_list: string[];
-	tags: string;
 	user: User;
 	organization?: Organization;
-	flare_tag?: FlareTag;
-}
-
-export interface FlareTag {
-	name: string;
-	bg_color_hex: String;
-	text_color_hex: String;
 }
 
 export interface Organization {
@@ -53,39 +44,27 @@ export interface User {
 	profile_image_90: string;
 }
 
-// TODO: unify
-export interface ApiArticleItem {
-	type_of: string;
-	id: number;
-	title: string;
-	description: string;
-	readable_publish_date: string;
-	slug: string;
-	path: string;
-	url: string;
-	comments_count: number;
-	public_reactions_count: number;
-	collection_id: null;
-	published_timestamp: string;
-	positive_reactions_count: number;
-	cover_image: string;
+export interface ApiArticleFeedItem extends ApiArticleCommon {
+	tag_list: string[];
+	tags: string;
+	flare_tag?: FlareTag;
+}
+
+export interface FlareTag {
+	name: string;
+	bg_color_hex: String;
+	text_color_hex: String;
+}
+
+export interface ApiArticleItem extends ApiArticleCommon {
 	cover_image_width: number;
 	cover_image_height: number;
 	cover_image_aspect_ratio: number;
-	social_image: string;
-	canonical_url: string;
-	created_at: string;
-	edited_at: null;
-	crossposted_at: null;
-	published_at: string;
-	last_comment_at: string;
-	reading_time_minutes: number;
 	tag_list: string;
 	tags: string[];
 	body_html: string;
 	body_markdown: string;
 	user: User;
-	organization: Organization;
 }
 
 export enum ArticleFeedApiStates {

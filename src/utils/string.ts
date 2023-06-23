1d36cbbd4bb917667d771fed613d3d3285d52352
Gowtham G <webappsbygowtham@gmail.com>
2023-06-23 10:46:09 +0530

    feat: add toTitleCase helper

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -5,3 +5,10 @@ export const replaceNewlines = (str: string, replaceValue: string): string => {
 
 	return str.replace(/(?:\r\n|\r|\n)/gm, replaceValue);
 };
+
+export const toTitleCase = (str?: string) => {
+	if (!str || str.length < 1) {
+		return str;
+	}
+	return `${str[0].toUpperCase()}${str.substring(1)}`;
+};

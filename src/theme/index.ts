0821e21ca13d712d64c16c92c06bfd5155ed196c
Gowtham G <webappsbygowtham@gmail.com>
2023-09-03 12:07:43 +0530

    fix: remove adapt theme and use custom colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,40 +1,21 @@
-import Iris from "./colors/iris";
-import {
-	DarkTheme as RNDarkTheme,
-	DefaultTheme as RNDefaultTheme,
-} from "@react-navigation/native";
+import Colors from "./colors/iris";
 import {
 	MD3DarkTheme,
 	MD3LightTheme,
 	MD3Theme,
-	adaptNavigationTheme,
 	configureFonts,
 } from "react-native-paper";
 
 import fontsConfig from "./fonts";
 
-const { LightTheme: AdpatedLightTheme, DarkTheme: AdpatedDarkTheme } =
-	adaptNavigationTheme({
-		reactNavigationLight: RNDefaultTheme,
-		reactNavigationDark: RNDarkTheme,
-	});
-
 export const LightTheme: MD3Theme = {
 	...MD3LightTheme,
-	...AdpatedLightTheme,
-	colors: {
-		...Iris.light,
-		...AdpatedLightTheme.colors,
-	},
+	colors: Colors.light,
 	fonts: configureFonts({ config: fontsConfig, isV3: true }),
 };
 
 export const DarkTheme: MD3Theme = {
 	...MD3DarkTheme,
-	...AdpatedDarkTheme,
-	colors: {
-		...Iris.dark,
-		...AdpatedDarkTheme.colors,
-	},
+	colors: Colors.dark,
 	fonts: configureFonts({ config: fontsConfig, isV3: true }),
 };
